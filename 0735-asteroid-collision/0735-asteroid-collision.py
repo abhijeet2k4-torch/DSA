9471bf59860c2d227b081846e15d0646c772e479
abhijeet2k4-torch <abhijeet2k4@gmail.com>
2026-07-01 22:20:14 +0530

    Time: 4 ms (94.09%), Space: 13.2 MB (76.58%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -9,12 +9,11 @@ class Solution(object):
             if n<0:
                     while stack and stack[-1] > 0 and stack[-1]<abs(n):
                         stack.pop()
-                    if not stack:
+                    if not stack or stack[-1] < 0:
                         stack.append(n)
                     elif (stack[-1]+n) == 0:
                         stack.pop()
-                    elif stack[-1] < 0:
-                        stack.append(n)
+                    
             else:
                 stack.append(n)     
         return stack           
