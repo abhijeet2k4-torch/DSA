dfa2bb5ff8f88f5a5dfef874224e7497a4d84b2b
abhijeet2k4-torch <abhijeet2k4@gmail.com>
2026-07-04 15:56:02 +0530

    Time: 133 ms (8.04%), Space: 16.1 MB (24.94%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -5,25 +5,23 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        remain = k
         kcount = 0
         kindex = deque()
         left = 0
         max_length = 0
-        for i in range(len(nums)):
-                if kcount == k:
-                    break
-                if nums[i] == 0:
-                    nums[i] = 1
-                    kcount += 1
-                    kindex.append(i)
         for right in range(len(nums)):
-            if nums[right] == 0 and kindex:
-                left = kindex[0] +1
-                kindex.popleft()
-                nums[right] = 1
-                kindex.append(right)
-            if nums[right] == 0 and not kindex:
+            if nums[right] == 0 and k == 0:
                 left = right+1
+            if nums[right] == 0 and remain>0:
+                nums[right] = 1
+                remain -= 1
+                kindex.append(right)
+            if nums[right] == 0 and remain == 0 and k != 0:
+                nums[right] = 1
+                left = kindex[0]+1
+                kindex.popleft()
+                kindex.append(right)
             max_length = max(max_length,(right-left+1))
         return max_length
 
