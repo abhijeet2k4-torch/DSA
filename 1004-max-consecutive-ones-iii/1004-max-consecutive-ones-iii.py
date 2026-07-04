0e988bf72f9106f11f62f836e3d84fd836995977
abhijeet2k4-torch <abhijeet2k4@gmail.com>
2026-07-04 15:59:28 +0530

    Time: 125 ms (9.8%), Space: 16.1 MB (24.94%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -11,17 +11,18 @@ class Solution(object):
         left = 0
         max_length = 0
         for right in range(len(nums)):
-            if nums[right] == 0 and k == 0:
-                left = right+1
-            if nums[right] == 0 and remain>0:
-                nums[right] = 1
-                remain -= 1
-                kindex.append(right)
-            if nums[right] == 0 and remain == 0 and k != 0:
-                nums[right] = 1
-                left = kindex[0]+1
-                kindex.popleft()
-                kindex.append(right)
+            if nums[right] == 0:
+                if k == 0:
+                    left = right+1
+                elif remain>0:
+                    nums[right] = 1
+                    remain -= 1
+                    kindex.append(right)
+                else:
+                    nums[right] = 1
+                    left = kindex[0]+1
+                    kindex.popleft()
+                    kindex.append(right)
             max_length = max(max_length,(right-left+1))
         return max_length
 
