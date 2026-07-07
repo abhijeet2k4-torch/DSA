f69d9fc6162527413a9108a323b2215de699b817
abhijeet2k4-torch <abhijeet2k4@gmail.com>
2026-07-07 15:13:51 +0530

    Time: 182 ms (43.91%), Space: 41.6 MB (44.09%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -2,9 +2,11 @@ class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         tlist = list(zip(position,speed))
         tlist.sort(reverse=True)
-        stack = []
+        count = 0
+        current_steps = 0
         for p,s in tlist:
-            steps = (target-p)/s
-            if not stack or stack[-1] < steps:
-                stack.append(steps)
-        return len(stack)
+            steps = float(target - p) / s
+            if current_steps < steps:
+                count += 1
+                current_steps = steps
+        return count
